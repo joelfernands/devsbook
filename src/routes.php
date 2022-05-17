5061ac64726a801128d337cdbf2aa3f3a86cb813
Joel Fernandes <joel.a.fernandes@outlook.com>
2022-05-16 21:18:09 -0300

    Iniciando no git

---
+++ src/routes.php
@@ -0,0 +1,14 @@
+<?php
+use core\Router;
+
+$router = new Router();
+
+$router->get('/', 'HomeController@index');
+
+$router->get('/login', 'LoginController@signin');
+$router->post('/login', 'LoginController@signinAction');
+
+$router->get('/add', 'LoginController@signup');
+$router->post('/add', 'LoginController@signupAction');
+
+
